44133972015316132c93f3a458e0def8f406e83f
Mohammed Sebehadin <mohammedsebehadin07@gmail.com>
2025-11-20 09:35:54 +0300

    Initialize student variables and update values

---
+++ variable.ts
@@ -0,0 +1,13 @@
+let nameStudent:string ='mohammed';
+var ageStudent:number=22;
+const country:string='Ethiopia';
+console.log('Name:',nameStudent);
+console.log('age:',ageStudent);
+console.log('country',country)
+//update the variable
+nameStudent='faris';
+ageStudent=21
+console.log('Name:',nameStudent)
+console.log('age:',ageStudent)
+
+
