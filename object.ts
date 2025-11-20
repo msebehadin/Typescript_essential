1d51ba04334e537cd6bda6a7ce7a8234b25dfa0b
Mohammed Sebehadin <mohammedsebehadin07@gmail.com>
2025-11-20 09:51:00 +0300

    adding the object and print them by console

---
+++ object.ts
@@ -0,0 +1,14 @@
+type Car={
+    brand:string;
+    model:String;
+    year:number
+}
+
+const car:Car={
+brand:'suzuki',
+model:'desire',
+year:2021
+}
+console.log(`brand:${car.brand}`)
+console.log(`model:${car.model}`)
+console.log(`year:${car.year}`)
